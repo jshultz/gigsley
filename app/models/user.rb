0b0b55768045d030f23f8e28b9bce4aebedb8740
YOUR NAME <jasshultz@gmail.com>
2016-04-03 16:06:25 -0600

    facebook login working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :invitable, :registerable, :validatable,
+  devise :database_authenticatable, :invitable, :registerable, :validatable, :omniauthable, :omniauth_providers => [:facebook],
          :validate_on_invite => false
-  devise :omniauthable, :omniauth_providers => [:facebook]
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-  validates :fullname, :email, presence: true, on: :create
+  # validates :name, :email, presence: true, on: :create
 
   def override_confirmable_email
     self.confirmed_at = DateTime.current
