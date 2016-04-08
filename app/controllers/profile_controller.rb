1354c23316d5b17451f13418365cd2c3990368ca
YOUR NAME <jasshultz@gmail.com>
2016-04-07 21:27:44 -0600

    profile updating and editing working

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,22 +5,41 @@ class ProfileController < ApplicationController
   end
 
   def show
+    @profile = Profile.where(user_id: params[:id].to_i).first
+    if @profile
+      @user = @profile.user
+    else
+      @user = nil
+    end
 
   end
 
   def edit
-
+    if current_user.id != params[:id].to_i
+      redirect_to profile_path
+    end
   end
 
   def update
-    byebug
-    @profile = @user.create_profile(params)
-    # need to handle *** ActiveModel::ForbiddenAttributesError Exception: ActiveModel::ForbiddenAttributesError
+    @user = User.where(id: current_user.id).first
+    @profile = @user.create_profile(profile_params)
   end
 
   def create
-    byebug
-    @profile = @user.create_profile(params)
-    # need to handle *** ActiveModel::ForbiddenAttributesError Exception: ActiveModel::ForbiddenAttributesError
+    @user = User.where(id: current_user.id).first
+    if @user.profile.nil?
+      @user.create_profile(profile_params)
+      redirect_to profile_path current_user.id
+    else
+      @user.profile.update_attributes(profile_params)
+      redirect_to profile_path current_user.id
+    end
+
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:street, :city, :state, :home_phone, :mobile_phone)
   end
 end
