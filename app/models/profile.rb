415ed20132fa1b259fabdaf7585afc18a4d1bc71
YOUR NAME <jasshultz@gmail.com>
2016-05-08 15:29:03 -0600

    working on messaging

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ActiveRecord::Base
   geocoded_by :full_address   # can also be an IP address
   after_validation :geocode #, if: ->(obj){ obj.full_address.present? and obj.full_address.changed? }
 
+  acts_as_messageable
   acts_as_ordered_taggable # Alias for acts_as_taggable_on :tags
   acts_as_ordered_taggable_on :skills, :interests
 
