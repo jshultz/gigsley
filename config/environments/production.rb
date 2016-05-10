a7d1ccead0d5cffcbe997e76c343734acffc0257
YOUR NAME <jasshultz@gmail.com>
2016-05-09 18:33:34 -0600

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -11,12 +11,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'http://calm-springs-53870.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
-      :user_name => ENV["SENDGRID_USERNAME"],
-      :password => ENV["SENDGRID_PASSWORD"],
-      :address => 'smtp.sendgrid.net',
-      :domain => 'myapp.herokuapp.com',
-      :port => 587,
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
       :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'calm-springs-53870.herokuapp.com',
       :enable_starttls_auto => true
   }
 
